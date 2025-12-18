0bf407e5aa65b9b957c6647834c8f3e254d5986a
nyxb <dennis@ollhoff.life>
2025-12-19 00:15:52 +0100

    feat: initial homebrew tap for belaf

---
+++ Formula/belaf.rb
@@ -0,0 +1,63 @@
+class Belaf < Formula
+  desc "Release management CLI for monorepos and multi-language projects"
+  homepage "https://github.com/ilblu/belaf"
+  version "0.1.0"
+  if OS.mac?
+    if Hardware::CPU.arm?
+      url "https://github.com/ilblu/belaf/releases/download/v0.1.0/belaf-aarch64-apple-darwin.tar.xz"
+      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+    end
+    if Hardware::CPU.intel?
+      url "https://github.com/ilblu/belaf/releases/download/v0.1.0/belaf-x86_64-apple-darwin.tar.xz"
+      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+    end
+  end
+  if OS.linux?
+    if Hardware::CPU.arm?
+      url "https://github.com/ilblu/belaf/releases/download/v0.1.0/belaf-aarch64-unknown-linux-gnu.tar.xz"
+      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+    end
+    if Hardware::CPU.intel?
+      url "https://github.com/ilblu/belaf/releases/download/v0.1.0/belaf-x86_64-unknown-linux-gnu.tar.xz"
+      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+    end
+  end
+  license "MIT"
+
+  BINARY_ALIASES = {
+    "aarch64-apple-darwin":      {},
+    "aarch64-unknown-linux-gnu": {},
+    "x86_64-apple-darwin":       {},
+    "x86_64-pc-windows-gnu":     {},
+    "x86_64-unknown-linux-gnu":  {},
+  }.freeze
+
+  def target_triple
+    cpu = Hardware::CPU.arm? ? "aarch64" : "x86_64"
+    os = OS.mac? ? "apple-darwin" : "unknown-linux-gnu"
+
+    "#{cpu}-#{os}"
+  end
+
+  def install_binary_aliases!
+    BINARY_ALIASES[target_triple.to_sym].each do |source, dests|
+      dests.each do |dest|
+        bin.install_symlink bin/source.to_s => dest
+      end
+    end
+  end
+
+  def install
+    bin.install "belaf" if OS.mac? && Hardware::CPU.arm?
+    bin.install "belaf" if OS.mac? && Hardware::CPU.intel?
+    bin.install "belaf" if OS.linux? && Hardware::CPU.arm?
+    bin.install "belaf" if OS.linux? && Hardware::CPU.intel?
+
+    install_binary_aliases!
+
+    doc_files = Dir["README.*", "readme.*", "LICENSE", "LICENSE.*", "CHANGELOG.*"]
+    leftover_contents = Dir["*"] - doc_files
+
+    pkgshare.install(*leftover_contents) unless leftover_contents.empty?
+  end
+end
---
+++ README.md
@@ -0,0 +1,14 @@
+# Homebrew Tap for belaf
+
+## Installation
+
+```bash
+brew tap ilblu/tap
+brew install belaf
+```
+
+## Update
+
+```bash
+brew upgrade belaf
+```
